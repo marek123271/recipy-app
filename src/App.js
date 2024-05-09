87df98c10ca1e462e7e4da37f70cb031b1e0c3a4
Marek Sroczyński <marek123271@gmail.com>
2024-05-09 14:26:21 +0100

    React router setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home.js';
+import Defoult from './pages/Defoult.js';
+import Recipies from './pages/Recipies.js';
+import SingleRecipe from './pages/SingleRecipe.js';
+
 
 function App() {
   return (
     <div>
-      Hello from app1
+      <Home></Home>
+      <Defoult></Defoult>
+      <Recipies></Recipies>
+      <SingleRecipe></SingleRecipe>
     </div>
   );
 }
