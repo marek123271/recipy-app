68cd2583de88a270bee8df9e8537ae7d342784c1
Marek Sroczyński <marek123271@gmail.com>
2024-05-09 14:26:21 +0100

    React router setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home.js';
+import Defoult from './pages/Defoult.js';
+import Recipes from './pages/Recipes.js';
+import SingleRecipe from './pages/SingleRecipe.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      Hello from app1
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/Recipes" exact element={<Recipes />} />
+        <Route path="/recipes/:id" element={<SingleRecipe />} />
+        <Route path="*" element={<Defoult />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Defoult.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Defoult extends Component {
+  render() {
+    return (
+      <h4>Hello from error page</h4>
+    )
+  }
+}
