08b27f49294162a004a74b75f4eed051a09e4f47
Marek Sroczyński <marek123271@gmail.com>
2024-05-11 13:28:28 +0100

    time hor hyperlink and przepis webite

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import './Recipe.css'; // Import CSS file for styling
 
 export default class Recipe extends Component {
   render() {
     const { recipe } = this.props;
 
     return (
-      <div>
+      <div className="recipe">
         <h2>{recipe.title}</h2>
-        <img src={recipe.image_url} alt={recipe.title} />
-        <p>{recipe.publisher}</p>
+        <img src={recipe.image_url} alt={recipe.title} className="recipe-image" />
+        
       </div>
     );
   }
