dfd7a8676c6e5c90f554fff5e3f79775022b6322
Marek Sroczyński <marek123271@gmail.com>
2024-05-11 14:07:11 +0100

    colour fonts

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -7,8 +7,9 @@ export default class Recipe extends Component {
 
     return (
       <div className="recipe">
-        <h2>{recipe.title}</h2>
+        <h2 className="orange-textT">{recipe.title}</h2>
         <img src={recipe.image_url} alt={recipe.title} className="recipe-image" />
+        <h6 className="orange-textC">provided by {recipe.publisher}</h6>
         
       </div>
     );
