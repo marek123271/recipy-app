4fb68387b9eba3a2abdd12fd41fd4808ba59e632
Marek Sroczyński <marek123271@gmail.com>
2024-05-12 11:05:28 +0100

    time for single recipe page

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import './Recipe.css'; // Import CSS file for styling
 
 export default class Recipe extends Component {
@@ -9,9 +10,11 @@ export default class Recipe extends Component {
       <div className="recipe">
         <h2 className="orange-textT">{recipe.title}</h2>
         <img src={recipe.image_url} alt={recipe.title} className="recipe-image" />
-        <h6 className="orange-textC">provided by {recipe.publisher}</h6>
-        
+        <h6 className="orange-textC">Provided by {recipe.publisher}</h6>
+        <div className='card-footer'>
+          <Link to={`/recipes/${recipe.id}`} className="btn btn-success">View Details</Link>
+        </div>
       </div>
     );
-  }
+}   
 }
